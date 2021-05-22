5b7c70128a945d705ca0a894899bf98061af8c72
Gregory Woolery <gregorywoolery1@gmail.com>
2021-05-22 14:54:50 -0500

    Update index.js

--- WUSH capacitor/www/js/index.js
+++ WUSH capacitor/www/js/index.js
@@ -173,3 +173,87 @@ function exit_strategy() { //when called twice exits the app
         }, 2000)
     }
 }
+
+
+// const speech = {
+//     data () {
+//       return {
+//         speechSentence: null,
+//         activatedSpeechRecognition: false,
+//         speechRecognitionListener: null,
+//         stopSpeechRecognitionListener: null
+//       }
+//     },
+//     methods: {
+//       startSpeechRecognition(speechSentence) {
+//         this.speechSentence = speechSentence;
+//         SpeechRecognition.hasPermission().then(permission => {
+//           if(permission.permission) {
+//             this.activateSpeechRecognition();
+//           } else {
+//             this.requestSpeechRecognitionPermission(true);
+//           }
+//         }).catch(() => {
+//           this.requestSpeechRecognitionPermission(true)
+//         })
+//       },
+//       requestSpeechRecognitionPermission(launch) {
+//         SpeechRecognition.requestPermission().then(() => {
+//           if (launch) {
+//             this.activateSpeechRecognition();
+//           }
+//         }).catch(() => {
+  
+//         });
+//       },
+//       activateSpeechRecognition() {
+//         this.activatedSpeechRecognition = true;
+//         this.speak(this.speechSentence.phrase);
+//         this.speechRecognitionListener = SpeechRecognition.addListener('speech-recognition-result', (res) => {
+//           if (this.speechSentence.results) {
+//             this.speechSentence.results.forEach(item => {
+//               if (item.matchings.some(match => res.matches[0].toLowerCase().includes(match))) {
+//                 this.speak(item.phrase);
+//                 item.callback();
+//                 this.stopSpeechRecognition();
+//               }
+//             })
+//           }
+//         });
+//         this.stopSpeechRecognitionListener = SpeechRecognition.addListener('speech-recognition-stopped', () => {
+//           this.stopSpeechRecognition()
+//         });
+//         SpeechRecognition.start({
+//           language: "fr-FR",
+//           maxResults: 1000,
+//           prompt: "Parlez",
+//           partialResults: true,
+//           popup: false,
+//         }).then(res => {
+  
+//         }).catch(() => {
+//           this.stopSpeechRecognition()
+//         });
+//       },
+//       stopSpeechRecognition() {
+//         this.activatedSpeechRecognition = false;
+//         if (this.speechRecognitionListener) {
+//           this.speechRecognitionListener.remove();
+//         }
+//         if (this.stopSpeechRecognitionListener) {
+//           this.stopSpeechRecognitionListener.remove();
+//         }
+//         SpeechRecognition.stop();
+//       },
+//       async speak(text) {
+//         await TextToSpeech.speak({
+//           text: text,
+//           lang: 'fr_FR',
+//           rate: 1.0,
+//           pitch: 1.0,
+//           volume: 1.0,
+//           category: 'ambient',
+//         });
+//       }
+//     }
+//   };
